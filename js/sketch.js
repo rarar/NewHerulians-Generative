4c6141c58625beb9e0a2c4bcb8b9864807b33412
rarar <me@rarar.com>
2020-08-08 16:11:36 -0700

    added textures, updated rules engine

--- js/sketch.js
+++ js/sketch.js
@@ -1,50 +1,90 @@
-let colorChoices = [
-  'rgba(149, 252, 145, 0.7)', // acceptance
-  'rgba(250, 20, 27, 0.7)', // anger
-  'rgba(252, 167, 92, 0.7)', // anticipation
-  'rgba(250, 97, 252, 0.7)', // disgust
-  'rgba(255, 252, 99, 0.7)', // joy
-  'rgba(34, 148, 23, 0.7)', // fear
-  'rgba(78, 91, 251, 0.7)', // sadness
-  'rgba(95, 191, 252, 0.7)' // surprise
-];
+// New Herulians Generative Engineer
+// Authors: Raphael Arar & Eric Liu
 
+let smoothImage, roughImage, ripplesImage, irregularImage;
+let bg;
+
+function preload() {
+  smoothImage = loadImage("../img/smooth.png");
+  roughImage = loadImage("../img/rough.png");
+  ripplesImage = loadImage("../img/ripples.png");
+  irregularImage = loadImage("../img/irregular.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  generateImprint(0, 1, 1);
+}
+
+function draw() {
+  lights();
+  generateImprint(5, 2, 3);
+  noLoop();
 }
 
 function generateImprint(q1, q2, q3) {
-
-    // background(0);
-  let c = random(colorChoices[q1]);
-  lights();
-
-  for (var i = 0; i < 100; i++) {
-    for (var j = 0; j < 100; j++) {
-      //rotateX(theta);
-      //rotateZ(theta + offset * i);
+  for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 15; j++) {
       push();
-      //texture(img);
-      noStroke();
-      ambientMaterial(colorChoices[q1]);
-      translate(i*random(-100,100), j * random(-100, 100));
+      translate(i*random(-50,50), j * random(-50, 50));
       rotateZ(random(360));
       rotateX(random(360));
       rotateY(random(360));
-      switch(3) {
+      noStroke();
+
+      switch(q3) {
         case 0:
-          sphere(random(35));
+          texture(smoothImage);
           break;
         case 1:
-          cone(random(35), random(35));
+          texture(roughImage);
           break;
         case 2:
-          cylinder(random(35), random(35));
+          texture(ripplesImage);
           break;
         case 3:
-          plane(random(35));
+          texture(irregularImage);
+          break;
+      }
+
+      switch(q1) {
+        case 0:
+          tint(149, 252, 145, 225);
+          break;
+        case 1:
+          tint(250, 20, 27, 225);
+          break;
+        case 2:
+          tint(252, 167, 92, 225);
+          break;
+        case 3:
+          tint(250, 97, 252, 225);
+          break;
+        case 4:
+          tint(255, 252, 99, 225);
+          break;
+        case 5:
+          tint(34, 148, 23, 225);
+          break;
+        case 6:
+          tint(78, 91, 251, 225);
+          break;
+        case 7:
+          tint(95, 191, 252, 225);
+          break;
+      }
+
+      switch(q2) {
+        case 0:
+          sphere(random(80));
+          break;
+        case 1:
+          cone(random(80), random(80));
+          break;
+        case 2:
+          cylinder(random(80), random(80));
+          break;
+        case 3:
+          plane(random(80));
           break;
       }
       pop();
